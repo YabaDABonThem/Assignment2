7dfe26d77f5fa772b1305a73ff8a512f8655ef12
Yabadaba <allenbao01@gmail.com>
2022-01-09 20:13:03 -0800

    finished writing classes, exit code 1 bug

--- AB.java
+++ AB.java
@@ -1,8 +1,34 @@
+import java.awt.Color;
+import java.util.Random;
+
 public class AB extends Critter {
+    private Color color;
+    private Random random;
 
     public AB() {
-
+        color = Color.BLUE;
+        random = new Random();
     }
 
+    public Color getColor() {
+        return color;
+    }
 
+    public String toString() {
+        return "AB";
+    }
+
+    public Action getMove(CritterInfo info) {
+        if (info.getFront() == Neighbor.OTHER) {
+            return Action.INFECT;
+        }
+        else if (info.getFront() == Neighbor.EMPTY) {
+            return Action.HOP;
+        }
+        // If you can't move forward then randomly turn left or right
+        else if (random.nextBoolean()) {
+            return Action.RIGHT;
+        }
+        return Action.LEFT;
+    }
 }
--- Giant.java
+++ Giant.java
@@ -1,2 +1,32 @@
+import java.awt.Color;
+
 public class Giant extends Critter{
+    private Color color;
+    public String[] stringOptions = {"fee", "fie", "foe", "fum"};
+    public int moveCounter;
+
+    public Giant() {
+        color = Color.GRAY;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public String toString() {
+        return stringOptions[moveCounter%24/6];
+    }
+
+    public Action getMove(CritterInfo info) {
+        ++moveCounter;
+
+        if (info.getFront() == Neighbor.OTHER) {
+            return Action.INFECT;
+        }
+        else if (info.getFront() == Neighbor.EMPTY) {
+            return Action.HOP;
+        }
+        return Action.RIGHT;
+
+    }
 }
